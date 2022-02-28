41745c47c6c5942a765fab298405e504e78386a9
Dima <zgltyq@gmail.com>
2022-02-28 16:36:32 +0200

    add install script

---
+++ install_requirements.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+curl -sL https://deb.nodesource.com/setup_16.x -o nodesource_setup.sh
+chmod 777 nodesource_setup.sh
+sudo ./nodesource_setup.sh
+sudo apt install nodejs
+npm install
